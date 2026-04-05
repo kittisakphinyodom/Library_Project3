ad861a1d871f609d6cbb45d58d98093e37fc8f87
Muse <66160133@go.buu.ac.th>
2026-04-05 20:14:49 +0700

    fix: add requireAdmin middleware to borrowing routes (fixes #1)

--- src/routes/borrowing.js
+++ src/routes/borrowing.js
@@ -5,16 +5,17 @@ const BorrowingController = require("../controllers/BorrowingController");
 const { requireAuth, requireAdmin } = require("../middleware/auth");
 
 router.get("/borrowed", requireAuth, BorrowingController.getBorrowed);
-router.get("/overdue", requireAuth, BorrowingController.getOverdue);
+router.get("/overdue", requireAuth, requireAdmin,BorrowingController.getOverdue);
 router.get(
   "/member/:memberId",
   requireAuth,
+  requireAdmin,
   BorrowingController.getMemberBorrows,
 );
-router.get("/", requireAuth, BorrowingController.getAll);
-router.post("/", requireAuth, BorrowingController.borrow);
+router.get("/", requireAuth, requireAdmin,BorrowingController.getAll);
+router.post("/", requireAuth, requireAdmin,BorrowingController.borrow);
 router.get("/:borrowId", requireAuth, BorrowingController.findById);
-router.put("/:borrowId/return", requireAuth, BorrowingController.returnBook);
+router.put("/:borrowId/return", requireAuth, requireAdmin, BorrowingController.returnBook);
 router.put("/:borrowId/extend", requireAuth, BorrowingController.extendDueDate);
 
 module.exports = router;
