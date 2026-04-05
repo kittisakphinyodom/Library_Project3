d827a577452dbeb725989c0939c48b1299f8ad45
kittisak <66160114@go.buu.ac.th>
2026-04-05 18:42:32 +0700

    fix Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,16 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs", 
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
